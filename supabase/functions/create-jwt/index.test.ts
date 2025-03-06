82281d126ef4a51a8d65adbf5a1a6c5acd1561d8
johnvonFMS <johnvon@futureproofmusicschool.com>
2025-03-06 14:31:28 -0800

    Delete Supabase

--- supabase/functions/create-jwt/index.test.ts
+++
@@ -1,7 +0,0 @@
-
-// Tests for the create-jwt function
-import { assertEquals } from "https://deno.land/std@0.168.0/testing/asserts.ts"
-
-Deno.test("create-jwt function", async () => {
-  // Add tests here when needed
-})
